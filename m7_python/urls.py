3b625eb36e27034511b653de901c56d69db560ac
NicolasJeldresL <nicolasjeldreslazoi@gmail.com>
2024-09-06 22:53:18 -0400

    first cmmit

--- m7_python/urls.py
+++ m7_python/urls.py
@@ -1,6 +1,12 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
+from . import views
 from .views import indexView
 
 urlpatterns = [
-    path('', indexView, name='home')
+    path('', indexView, name='home'),
+     path('login/', auth_views.LoginView.as_view(), name='login'),  # Login page
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Logout page
+    path('register/', views.register, name='register'),  # Register page (your custom view)
 ]
+
--- m7_python/views.py
+++ m7_python/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .services import get_all_inmuebles
 
 # Create your views here.
 def indexView(request):
     inmuebles = get_all_inmuebles()
     return render(request,'index.html',{'inmuebles':inmuebles} )
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after registration
+            return redirect('index')  # Redirect to the home page
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
